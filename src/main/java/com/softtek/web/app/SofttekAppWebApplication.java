a5dc5413c31342353d93111b120d64030bdd953e
atmdamian <sudodamian@hotmail.com>
2020-11-17 13:34:03 -0600

    cambio de empaquetado war

--- src/main/java/com/softtek/web/app/SofttekAppWebApplication.java
+++ src/main/java/com/softtek/web/app/SofttekAppWebApplication.java
@@ -1,19 +1,20 @@
 package com.softtek.web.app;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SofttekAppWebApplication {
+public class SofttekAppWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SofttekAppWebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SofttekAppWebApplication.class, args);
 	}
-
-
 
 }
